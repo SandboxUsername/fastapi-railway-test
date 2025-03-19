a82c6fcdf4fbd4c099420532ee35f035c840b39f
César Boria <cesar.boria.j@gmail.com>
2025-03-19 01:34:43 +0100

    Second endpoint added!

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello from Railway!"}
+
+@app.get("/second_endpoint")
+def second_endpoint():
+    return {"message": "This is a second endpoint!"}
